529c86440e6aaebd8ad98979ae781f7f57ff72ef
sumedhtirodkar1508 <sumedhtirodkar15@gmail.com>
2025-01-18 10:04:20 -0500

    Ninth commit fixing next config caching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)", // Apply to all API routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0", // No cache, always fetch fresh data
+          },
+          {
+            key: "Pragma",
+            value: "no-cache", // Legacy header
+          },
+          {
+            key: "Expires",
+            value: "0", // Immediately expired
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
